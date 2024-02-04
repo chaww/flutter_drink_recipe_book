d7c15e268264dc5dd051b9fbc2c04a34717b20c9
chaww <chatchawan.wc@gmail.com>
2024-02-05 04:01:47 +0700

    update global_providers

--- lib/global_providers.dart
+++ lib/global_providers.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_drink_recipe_book/data/repositories/menu_repository.mock.dart';
+import 'package:flutter_drink_recipe_book/data/states/menu/menu_event.dart';
 import 'package:flutter_drink_recipe_book/data/states/settings/settings_bloc.dart';
+import 'package:flutter_drink_recipe_book/data/states/menu/menu_bloc.dart';
 
 class GlobalBlocProviders extends StatelessWidget {
   final Widget child;
@@ -10,15 +13,44 @@ class GlobalBlocProviders extends StatelessWidget {
     required this.child,
   });
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildMultiBlocProvider({
+    required Widget child,
+  }) {
     return MultiBlocProvider(
       providers: [
         BlocProvider<SettingsBloc>(
           create: (context) => SettingsBloc(),
-        )
+        ),
+        BlocProvider<MenuBloc>(
+          lazy: false,
+          create: (context) => MenuBloc(
+            menuRepository: context.read<MenuMockRepository>(),
+          )..add(const SubscriptionMenuList()),
+        ),
       ],
       child: child,
+    );
+  }
+
+  Widget _buildMultiRepositoryProvider({
+    required Widget child,
+  }) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<MenuMockRepository>(
+          create: (context) => MenuMockRepository(),
+        ),
+      ],
+      child: child,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildMultiRepositoryProvider(
+      child: _buildMultiBlocProvider(
+        child: child,
+      ),
     );
   }
 }
