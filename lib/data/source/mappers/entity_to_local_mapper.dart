1f2c170f591b38ce4b7370d3e5829574ade42792
chaww <chatchawan.wc@gmail.com>
2024-02-12 02:39:01 +0700

    add mappers for Hive

---
+++ lib/data/source/mappers/entity_to_local_mapper.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_drink_recipe_book/data/entities/ingredient.dart';
+import 'package:flutter_drink_recipe_book/data/entities/menu.dart';
+import 'package:flutter_drink_recipe_book/data/entities/recipe.dart';
+import 'package:flutter_drink_recipe_book/data/source/local_datasource/models/ingredient.dart';
+import 'package:flutter_drink_recipe_book/data/source/local_datasource/models/menu.dart';
+import 'package:flutter_drink_recipe_book/data/source/local_datasource/models/recipe.dart';
+
+extension MenuHiveModelX on Menu {
+  MenuHiveModel toHiveModel() => MenuHiveModel()
+    ..id = id
+    ..nameTh = nameTh
+    ..nameEn = nameEn
+    ..imageSrc = imageSrc
+    ..category = category
+    ..recipesHot = recipesHot.map((e) => e.toHiveModel()).toList()
+    ..recipesIce = recipesIce.map((e) => e.toHiveModel()).toList()
+    ..recipesFrappe = recipesFrappe.map((e) => e.toHiveModel()).toList();
+}
+
+extension RecipeHiveModelX on Recipe {
+  RecipeHiveModel toHiveModel() => RecipeHiveModel()
+    ..optionName = optionName
+    ..ingredients = ingredients.map((e) => e.toHiveModel()).toList();
+}
+
+extension IngredientHiveModelX on Ingredient {
+  IngredientHiveModel toHiveModel() => IngredientHiveModel()
+    ..name = name
+    ..value = value
+    ..unit = unit;
+}
---
+++ lib/data/source/mappers/local_to_entity_mapper.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_drink_recipe_book/data/entities/ingredient.dart';
+import 'package:flutter_drink_recipe_book/data/entities/menu.dart';
+import 'package:flutter_drink_recipe_book/data/entities/recipe.dart';
+import 'package:flutter_drink_recipe_book/data/source/local_datasource/models/ingredient.dart';
+import 'package:flutter_drink_recipe_book/data/source/local_datasource/models/menu.dart';
+import 'package:flutter_drink_recipe_book/data/source/local_datasource/models/recipe.dart';
+
+extension MenuHiveModelX on MenuHiveModel {
+  Menu toEntity() => Menu(
+        id: id,
+        nameTh: nameTh,
+        nameEn: nameEn,
+        imageSrc: imageSrc,
+        category: category,
+        recipesHot: recipesHot.map((e) => e.toEntity()).toList(),
+        recipesIce: recipesIce.map((e) => e.toEntity()).toList(),
+        recipesFrappe: recipesFrappe.map((e) => e.toEntity()).toList(),
+      );
+}
+
+extension RecipeHiveModelX on RecipeHiveModel {
+  Recipe toEntity() => Recipe(
+        optionName: optionName,
+        ingredients: ingredients.map((e) => e.toEntity()).toList(),
+      );
+}
+
+extension IngredientHiveModelX on IngredientHiveModel {
+  Ingredient toEntity() => Ingredient(
+        name: name,
+        value: value,
+        unit: unit,
+      );
+}
